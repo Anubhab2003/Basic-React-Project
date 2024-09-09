3190506e63ab389b2538ce8a8fa900dd3955b4eb
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-09-09 19:49:03 +0530

    tic-tac-toe

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
-import React from 'react';
-import Header from './Components/Header';
-import Balance from './Components/Balance';
-import IncomeExpense from './Components/IncomeExpense';
-import TransactionList from './Components/TransactionList';
-import Transaction from './Components/AddTransaction';
+import "./App.css";
+import React from "react";
+import Header from "./Components/Header";
+import Balance from "./Components/Balance";
+import IncomeExpense from "./Components/IncomeExpense";
+import TransactionList from "./Components/TransactionList";
+import Transaction from "./Components/AddTransaction";
 
-import { GlobalProvider } from './Context/GlobalState'; 
+import { GlobalProvider } from "./Context/GlobalState";
 
 function App() {
   return (
-    <GlobalProvider>
-    <Header />
-    <div classNameName="container">
-      <Balance />
-      <IncomeExpense />
-      <TransactionList />
-      <Transaction />
+    <div className="p-1 border-cyan-400">
+      <GlobalProvider>
+        <Header />
+        <div classNameName="container">
+          <Balance />
+          <IncomeExpense />
+          <TransactionList />
+          <Transaction />
+        </div>
+      </GlobalProvider>
     </div>
-
-    </GlobalProvider>
   );
 }
 
